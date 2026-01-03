25fe5520b864e33ead79d99bf6ec9bb55195dfac
stibu81 <slanz1137@gmail.com>
2026-01-03 15:43:54 +0100

    initial commit

---
+++ app.R
@@ -0,0 +1,7 @@
+library(simpleTaxonomy)
+
+run_taxonomy(
+  file = paste0("https://raw.githubusercontent.com/",
+                "stibu81/taxonomyData/refs/heads/main/taxonomy.csv"),
+  image_size = 200
+)
