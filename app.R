58c37120dd29886239ce43282c1621c788475b3a
stibu81 <slanz1137@gmail.com>
2026-03-07 22:28:27 +0100

    update simpleTaxonomy

--- app.R
+++ app.R
@@ -3,9 +3,10 @@ library(simpleTaxonomy)
 # here in order to be included in manifest.json
 library(DT)
 library(logger)
+library(shinyWidgets)
 
 run_taxonomy(
   file = paste0("https://raw.githubusercontent.com/",
                 "stibu81/taxonomyData/refs/heads/main/taxonomy.csv"),
-  image_size = 200
+  image_size = "200"
 )
