4023ab70eeb40a32279c0873c79f82b841697bf0
stibu81 <slanz1137@gmail.com>
2026-04-20 20:46:52 +0200

    remove unsupported image size of 200px

--- app.R
+++ app.R
@@ -8,5 +8,5 @@ library(shinyWidgets)
 run_taxonomy(
   file = paste0("https://raw.githubusercontent.com/",
                 "stibu81/taxonomyData/refs/heads/main/taxonomy.csv"),
-  image_size = "200"
+  image_size = "250"
 )
