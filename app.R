f49cd616167fb7db5f1829996c19e944da4b358d
stibu81 <slanz1137@gmail.com>
2026-08-25 21:55:48 +0200

    add Switzerland as local country

--- app.R
+++ app.R
@@ -8,5 +8,6 @@ library(shinyWidgets)
 run_taxonomy(
   file = paste0("https://raw.githubusercontent.com/",
                 "stibu81/taxonomyData/refs/heads/main/taxonomy.csv"),
-  image_size = "250"
+  image_size = "250",
+  local_country = "ch"
 )
